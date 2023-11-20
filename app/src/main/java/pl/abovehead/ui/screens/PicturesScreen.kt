f7130c6b2bfd8794b623a7f4c69fb526f9fb5504
Mateusz Dudkowski <mateusz.dudkowski.mobiledev@gmail.com>
2023-11-20 20:39:49 +0100

    First data from abovehead.pl via graphql

--- app/src/main/java/pl/abovehead/ui/screens/PicturesScreen.kt
+++ app/src/main/java/pl/abovehead/ui/screens/PicturesScreen.kt
@@ -3,7 +3,6 @@ package pl.abovehead.ui.screens
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,11 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -25,27 +22,42 @@ import pl.abovehead.apolloClient
 
 @Composable
 fun PicturesScreen() {
-    var responseState by rememberSaveable { mutableStateOf("") }
+    var responseState by rememberSaveable { mutableStateOf<GetAstrophotosQuery.Data?>(null) }
     LaunchedEffect(Unit) {
         val response = apolloClient.query(GetAstrophotosQuery("cG9zdDoxMTA=")).execute()
         Log.d("LOL", "CartScreen: ${response.data}")
-        responseState.plus(response)
+        responseState = response.data!!
     }
 
     Surface(color = MaterialTheme.colorScheme.background) {
-        Text(stringResource(id = R.string.pictures))
+        if (responseState!= null) ImagesWithTitlesList(responseState!!)
     }
 }
 
-//@Composable
-//fun ImagesWithTitlesList(){
-//    LazyColumn {
-//        items() {
-//            AsyncImage(
-//                modifier = Modifier.size(160.dp, 160.dp),
-//                model = data.launch?.mission?.missionPatch,
-//                contentDescription = "Mission patch"
-//            )
-//        }
-//    }
-//}
+@Composable
+fun ImagesWithTitlesList(data: GetAstrophotosQuery.Data) {
+    val cosik = data.page!!.blocks[1]!!.innerHtml
+    val url = parseImg(cosik!!)
+    Column {
+        Text(stringResource(id = R.string.pictures))
+        AsyncImage(
+            modifier = Modifier.size(160.dp, 160.dp),
+            model = url,
+            contentDescription = "Mission patch"
+        )
+
+    }
+}
+
+private fun parseImg(innerHtml: String): String {
+    var substr = innerHtml
+
+    val startIndex: Int = substr.indexOf("<a href=")
+    if (startIndex != -1) {
+        substr = substr.substring(startIndex + 9)
+        val urlIndex = substr.indexOf("\"")
+        substr = substr.substring(0, urlIndex)
+    }
+
+    return substr
+}
