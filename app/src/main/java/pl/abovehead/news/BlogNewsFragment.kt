7254199476dff4623e0186a9640f195173a548e4
Mateusz Dudkowski <mateusz.dudkowski.mobiledev@gmail.com>
2023-12-27 15:02:46 +0100

    Change BlogPost Fragment to use Compose instead of XML

--- app/src/main/java/pl/abovehead/news/BlogNewsFragment.kt
+++ app/src/main/java/pl/abovehead/news/BlogNewsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -15,35 +16,14 @@ import pl.abovehead.news.model.RssItem
 
 @AndroidEntryPoint
 class BlogNewsFragment : Fragment() {
-
-    companion object {
-        @BindingAdapter("listItems")
-        @JvmStatic
-        fun bindRecyclerView(recyclerView: RecyclerView, items: List<RssItem>?) {
-            val adapter = recyclerView.adapter as? RssFeedAdapter
-            adapter?.submitList(items)
-        }
-    }
-
-    private lateinit var viewModel: NASANewsViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: FragmentNewsBinding =
-            FragmentNewsBinding.inflate(inflater, container, false)
-        val view = binding.root
-
-        val adapter = RssFeedAdapter()
-        binding.recyclerView.layoutManager = LinearLayoutManager(context)
-        binding.recyclerView.adapter = adapter
-
-        viewModel = ViewModelProvider(this)[NASANewsViewModel::class.java]
-
-        viewModel.rssItemsLiveData.observe(viewLifecycleOwner) { rssItems ->
-            adapter.submitList(rssItems)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                PostsList()
+            }
         }
-
-        return view
     }
 }
