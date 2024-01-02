b77d862cdda665fc6e58e0b27492bacfeadf835a
mateuszdudkowski <mdudkowski@sii.pl>
2024-01-02 15:26:03 +0100

    Polishing details screens

--- app/src/main/java/pl/abovehead/news/ui/NewsDetails.kt
+++ app/src/main/java/pl/abovehead/news/ui/NewsDetails.kt
@@ -1,6 +1,5 @@
 package pl.abovehead.news.ui
 
-import android.text.Html
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,9 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.text.HtmlCompat
 import coil.compose.AsyncImage
 import com.google.android.material.textview.MaterialTextView
 import pl.abovehead.news.domain.Post
@@ -28,23 +29,31 @@ fun NewsDetails(post: Post?) {
     AboveHeadTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
         ) {
-            Column(modifier = Modifier
-                .padding(16.dp)
-                .verticalScroll(rememberScrollState())) {
-                Text(text = post?.title ?: "")
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Justify,
+                    fontSize = 24.sp,
+                    text = post?.title ?: ""
+                )
                 AsyncImage(
-                    model = post?.imageUrl,
-                    contentDescription = post?.title
+                    modifier = Modifier.padding (8.dp),
+                    model = post?.imageUrl, contentDescription = post?.title
                 )
                 AndroidView(
-                    factory = { MaterialTextView(it) },
-                    update = {
-                        it.text = post?.description
-                    }
-                )
+                    modifier = Modifier.padding (8.dp),
+                    factory = { MaterialTextView(it) }, update = {
+                    it.text = post?.description
+                })
                 Button(onClick = { uriHandler.openUri(post?.link ?: "https:www.abovehead.pl") }) {
                     Text(text = "More details")
                 }
