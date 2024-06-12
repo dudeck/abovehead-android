9ce65b12ac12d53a3d273bc9d2e1e9cad209b37a
Mateusz Dudkowski <mateusz.dudkowski.mobiledev@gmail.com>
2024-06-12 14:26:44 +0200

    Added to cart toast + new version 11 (1.0)

--- app/src/main/java/pl/abovehead/pictures/ui/TabLayout.kt
+++ app/src/main/java/pl/abovehead/pictures/ui/TabLayout.kt
@@ -1,5 +1,6 @@
 package pl.abovehead.pictures.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -57,7 +59,9 @@ fun PicturesTabLayout(
 
 @Composable
 fun AddToCartButton(addOrder: (OrderItem) -> Unit, title: String, image: String?) {
+   val context = LocalContext.current
     FloatingActionButton(modifier = Modifier.padding(36.dp), onClick = {
+        Toast.makeText(context, R.string.added_to_cart, Toast.LENGTH_SHORT).show()
         addOrder(OrderItem(title = title, image = image))
     }) {
         Icon(Icons.Filled.Add, stringResource(R.string.add_to_cart))
