0a7c0a3bcc4c0b738e22da64352dffc7c2fabc70
Mateusz Dudkowski <mateusz.dudkowski.mobiledev@gmail.com>
2024-09-11 15:51:08 +0200

    Adjust UI changes for blog news lists

--- app/src/main/java/pl/abovehead/news/ui/PostsList.kt
+++ app/src/main/java/pl/abovehead/news/ui/PostsList.kt
@@ -3,10 +3,12 @@ package pl.abovehead.news.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -103,6 +105,7 @@ private fun PostItem(post: Post, navController: NavController) {
                     color = Color.White,
                     text = post.title.uppercase(Locale.ROOT)
                 )
+                Spacer(modifier = Modifier.size(height = 16.dp, width = 8.dp))
                 Text(
                     modifier = Modifier
                         .padding(horizontal = 8.dp)
