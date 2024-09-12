cfae943ff3d02b26d257425505c102433a8b0f8a
Mateusz Dudkowski <mateusz.dudkowski.mobiledev@gmail.com>
2024-09-12 16:17:49 +0200

    Rotation screen in GalleryView

--- app/src/main/java/pl/abovehead/pictures/AstroPhotoGallery.kt
+++ app/src/main/java/pl/abovehead/pictures/AstroPhotoGallery.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -81,7 +82,7 @@ fun GalleryView(addOrder: (OrderItem) -> Unit, state: State<PicturesState>) {
 
         is ApplicationError -> ErrorMessage(data.errors[0].message)
         is Success -> {
-            var selectedImage by remember { mutableStateOf<Picture?>(null) }
+            var selectedImage by rememberSaveable { mutableStateOf<Picture?>(null) }
             val mContext = LocalContext.current
             if (selectedImage != null) {
                 // Display full-screen image
