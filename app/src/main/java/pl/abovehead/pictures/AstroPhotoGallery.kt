19425026346bad39f68ba7442eafc3bcc5412e15
Mateusz Dudkowski <mateusz.dudkowski.mobiledev@gmail.com>
2024-09-12 15:53:16 +0200

    Zooming properly

--- app/src/main/java/pl/abovehead/pictures/AstroPhotoGallery.kt
+++ app/src/main/java/pl/abovehead/pictures/AstroPhotoGallery.kt
@@ -1,11 +1,17 @@
 package pl.abovehead.pictures
 
 import android.content.Intent
+import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -17,11 +23,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.isFinite
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -53,6 +63,14 @@ fun AstroPhotoGallery(addOrder: (OrderItem) -> Unit, galleryViewModel: GalleryVi
 
 @Composable
 fun GalleryView(addOrder: (OrderItem) -> Unit, state: State<PicturesState>) {
+
+    var scale by remember {
+        mutableFloatStateOf(1f)
+    }
+    var offset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
     when (val data = state.value) {
         PicturesState.Loading -> Loading()
         is ProtocolError -> ErrorMessage(
@@ -67,27 +85,57 @@ fun GalleryView(addOrder: (OrderItem) -> Unit, state: State<PicturesState>) {
             val mContext = LocalContext.current
             if (selectedImage != null) {
                 // Display full-screen image
-                AsyncImage(
-                    model = selectedImage!!.url,
-                    contentDescription = null,
+                BoxWithConstraints(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(8.dp)
-                        .clickable {
-                            val picture = selectedImage
-                            selectedImage = null
-                            startActivity(
-                                mContext,
-                                Intent(mContext, PictureDetailsActivity::class.java).apply {
-                                    putExtra(
-                                        "item",
-                                        picture
-                                    )
-                                },
-                                null
+                        .fillMaxWidth()
+//            .aspectRatio(1280f / 959f)
+                ) {
+                    val transformableState =
+                        rememberTransformableState { zoomChange, panChange, rotationChange ->
+                            scale = (scale * zoomChange).coerceIn(1f, 5f)
+
+                            val extraWidth = (scale - 1) * constraints.maxWidth
+                            val extraHeight = (scale - 1) * constraints.maxHeight
+
+                            val maxX = extraWidth / 2
+                            val maxY = extraHeight / 2
+
+                            offset = Offset(
+                                x = (offset.x + scale * panChange.x).coerceIn(-maxX, maxX),
+                                y = (offset.y + scale * panChange.y).coerceIn(-maxY, maxY),
                             )
-                        } // Close on click
-                )
+                        }
+                    AsyncImage(
+                        model = selectedImage!!.url,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp)
+                            .graphicsLayer {
+                                scaleX = scale
+                                scaleY = scale
+                                translationX = offset.x
+                                translationY = offset.y
+                            }
+                            .transformable(transformableState)
+                            .clickable {
+                                val picture = selectedImage
+                                selectedImage = null
+                                scale = 1f
+                                offset = Offset.Zero
+                                startActivity(
+                                    mContext,
+                                    Intent(mContext, PictureDetailsActivity::class.java).apply {
+                                        putExtra(
+                                            "item",
+                                            picture
+                                        )
+                                    },
+                                    null
+                                )
+                            } // Close on click
+                    )
+                }
             } else {
                 // Display the gallery
                 LazyVerticalStaggeredGrid(
